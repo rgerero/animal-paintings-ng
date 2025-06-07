cb19ef7f71e46c8c677ee1b01c4518724e1fa170
alfredryangerero <alfredrrg-v@crimsonlogic.com>
2025-06-07 21:00:26 +0800

    calculate the total price

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -10,14 +10,21 @@ import { Product } from 'src/app/models/product';
 export class CartViewComponent implements OnInit{
 
   cartitems: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService){
 
   }
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
-      this.cartitems = data
+      this.cartitems = data;
+      this.totalPrice = this.getTotalPrice();
     })
+  }
+
+  getTotalPrice(): number {
+    this.totalPrice = this.cartitems.reduce((sum, item) => sum + item.price, 0);
+    return this.totalPrice;
   }
 
 }
