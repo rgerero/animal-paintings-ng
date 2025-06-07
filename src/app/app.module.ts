73da394bced96ec3f984256eabd4506b8dc2251d
alfredryangerero <alfredrrg-v@crimsonlogic.com>
2025-06-07 10:02:25 +0800

    add the missing cart module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { CartViewComponent } from './cart/cart-view/cart-view.component'; 
+import { CartModule } from './cart/cart/cart.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CartViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +22,8 @@ import { CartViewComponent } from './cart/cart-view/cart-view.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    CartModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
